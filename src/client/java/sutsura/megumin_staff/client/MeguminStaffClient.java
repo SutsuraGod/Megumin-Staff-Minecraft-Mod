51eee47ec19c1cf563f89e32558130b18f5ffa1f
sutsura <sutsura0123@gmail.com>
2026-05-04 10:41:54 +0300

    init commit

---
+++ src/client/java/sutsura/megumin_staff/client/MeguminStaffClient.java
@@ -0,0 +1,10 @@
+package sutsura.megumin_staff.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MeguminStaffClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
